c910123121c626a0c36384967b1f685249a62967
parate362 <144500446+parate362@users.noreply.github.com>
2024-06-26 12:52:49 +0530

    Update server.js

--- Keek-Server/server.js
+++ Keek-Server/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: true, // Allow all origins
+    origin: ["http://localhost:3000", "https://keek-nine.vercel.app"], // Add your new origin here
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
     allowedHeaders: 'Content-Type,Authorization',
