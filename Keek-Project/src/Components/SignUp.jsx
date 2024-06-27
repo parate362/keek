af13d92a414c4f0654acbae815af85ab81446461
parate362 <144500446+parate362@users.noreply.github.com>
2024-06-27 09:24:53 +0530

    Update SignUp.jsx

--- Keek-Project/src/Components/SignUp.jsx
+++ Keek-Project/src/Components/SignUp.jsx
@@ -97,7 +97,7 @@ const SignUp = () => {
 
   const sendOtp = async () => {
     try {
-      const response = await fetch(`https://keek-ten.vercel.app/api/user/send-otp`, {
+      const response = await fetch(`https://keek-server.vercel.app/api/user/send-otp`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -127,7 +127,7 @@ const SignUp = () => {
   const verifyOtp = async () => {
    
     try {
-      const response = await fetch(`https://keek-ten.vercel.app/api/user/verify-email`, {
+      const response = await fetch(`https://keek-server.vercel.app/api/user/verify-email`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -209,7 +209,7 @@ const SignUp = () => {
     // updatedFormData.isSubmitting = true; 
     // setFormData(updatedFormData);
     
-    axios.post("https://keek-ten.vercel.app/api/user/register-email", formData)
+    axios.post("https://keek-server.vercel.app/api/user/register-email", formData)
       .then((response) => {
         setSuccess('Sign up successful!');
         setError('');
